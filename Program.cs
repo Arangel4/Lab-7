c964baa6fb125ec591a1d799cd0476b6f66ba2be
Ariana Rangel <arianarangel97@gmail.com>
2019-02-26 09:34:20 -0600

    Final Commit

--- Program.cs
+++ Program.cs
@@ -7,18 +7,22 @@ namespace Lab_7
 {
     class Program
     {
-        using (var db = new Lab_7Context())
+        //  The List() method lists our the course and the students in the course in a neat format.
+        static void List()
         {
-            var allStuff = db.courses.Include(c => c.StudentCourses).ThenInclude(s => s.Student);
-
-            foreach (var course in allStuff)
+            using (var db = new Lab_7Context())
             {
-                Console.WriteLine($"{course.Name} -");
-                foreach (var student in course.StudentCourses)
+                var allStuff = db.Courses.Include(c => c.StudentCourses).ThenInclude(s => s.Student);
+
+                foreach (var course in allStuff)
                 {
-                    Console.WriteLine($"\t{student.Student.FirstName} {student.Student.LastName} {student.GPA}");
+                    Console.WriteLine($"{course.Name}");
+                    foreach (var student in course.StudentCourses)
+                    {
+                        Console.WriteLine($"\t{student.Student.FirstName} {student.Student.LastName}, {student.GPA:N}");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
             }
         }
         static void Main(string[] args)
@@ -28,12 +32,14 @@ namespace Lab_7
                 db.Database.EnsureDeleted();
                 db.Database.EnsureCreated();
 
+                //  The courses in the database.
                 List<Course> courses = new List<Course>()
                 {
                     new Course {Name = "Programming Business Applications"},
                     new Course {Name = "Software Systems Development"}
                 };
 
+                //  The students in the database.
                 List<Student> students = new List<Student>()
                 {
                     new Student {FirstName = "Ariana", LastName = "Rangel"},
@@ -42,21 +48,46 @@ namespace Lab_7
                     new Student {FirstName = "Tomas", LastName = "Sanchez"}
                 };
 
+                //  Connects the students with which course they are in as well as their GPA.
                 List<StudentCourse> joinedTable = new List<StudentCourse>()
                 {
                     new StudentCourse {Student = students[0], Course = courses[0], GPA = 3.5},
-                    new StudentCourse {Student = students[1], Course = courses[0], GPA = 3.7},
+                    new StudentCourse {Student = students[1], Course = courses[0], GPA = 4.0},
                     new StudentCourse {Student = students[2], Course = courses[0], GPA = 3.6},
-                    new StudentCourse {Student = students[3], Course = courses[0], GPA = 3.0},
-                    new StudentCourse {Student = students[0], Course = courses[1], GPA = 3.5},
                     new StudentCourse {Student = students[1], Course = courses[1], GPA = 3.7},
-                    new StudentCourse {Student = students[2], Course = courses[1], GPA = 3.6},
                     new StudentCourse {Student = students[3], Course = courses[1], GPA = 3.0},
                 };
 
                 db.AddRange(students);
                 db.AddRange(courses);
                 db.AddRange(joinedTable);
+                db.SaveChanges();
+            }
+            List();
+
+            //  Removes a student (Tomas Sanchez) from one of the courses (Software Systems Development). 
+            //  The student still exists but is no longer enrolled in the course.
+            using (var db = new Lab_7Context())
+            {
+                int studentID = 4;
+                int courseID = 2;
+
+                StudentCourse studentRemove = db.StudentCourses.Find(studentID, courseID);
+                Student s = db.Students.Find(studentID);
+                Course c = db.Courses.Find(courseID);
+                db.Remove(studentRemove);
+                db.SaveChanges();
+            }
+
+            //  Adds a new transfer student (Julian Garcia) and enrolls that student in one course (Programming Business Applications).
+            using (var db = new Lab_7Context())
+            {
+                Student transferStudent = new Student();
+                transferStudent.FirstName = "Julian";
+                transferStudent.LastName = "Garcia";
+                StudentCourse transferCourse = new StudentCourse {Student = transferStudent, CourseID = 1, GPA = 2.7};
+                db.Add(transferStudent);
+                db.Add(transferCourse);
                 db.SaveChanges();
             }
             List();
